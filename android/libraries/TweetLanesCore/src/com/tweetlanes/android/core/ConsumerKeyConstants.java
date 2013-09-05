42261cc661605c0844c753bfe9becae7b37f5151
Andrew Neal <andrewdneal@gmail.com>
2013-09-05 18:20:44 -0500

    Update .gitignore

--- android/libraries/TweetLanesCore/src/com/tweetlanes/android/core/ConsumerKeyConstants.java
+++ android/libraries/TweetLanesCore/src/com/tweetlanes/android/core/ConsumerKeyConstants.java
@@ -1,12 +1,17 @@
+
 package com.tweetlanes.android.core;
 
-public class ConsumerKeyConstants {
-    // Create a new key/secret here: https://dev.twitter.com/apps and here: https://account.app.net/developer/app/create/
-    // Update below, uncomment lines:
+@SuppressWarnings("javadoc")
+public final class ConsumerKeyConstants {
 
-    //public static final String TWITTER_CONSUMER_KEY = "Put your key here";
-    //public static final String TWITTER_CONSUMER_SECRET = "Put your secret here";
-    //public static final String APPDOTNET_CONSUMER_KEY = "Put your key here";
-    //public static final String APPDOTNET_CONSUMER_SECRET = "Put your secret here";
+    /* This class is never initialized */
+    private ConsumerKeyConstants() {
+    }
+
+    public static final String TWITTER_CONSUMER_KEY = "ixaZxRLfpPW7rk1QZyym0Q";
+    public static final String TWITTER_CONSUMER_SECRET = "WGxzhWC8NwesWzQbvCdc9XBHjbaXBFOqYa09LS8xY";
+    public static final String APPDOTNET_CONSUMER_KEY = "Put your key here";
+    public static final String APPDOTNET_CONSUMER_SECRET = "Put your secret here";
     public static final String CRITTERCISM_APP_ID = "4f6d53e0b093157b43000033";
+
 }
