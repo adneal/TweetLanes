77b2890b1e3ac7f98fded73d44b140a223a7ec65
Andrew Neal <andrewdneal@gmail.com>
2013-09-06 07:29:20 -0500

    whatevs, yo

---
+++ android/libraries/TweetLanesCore/src/com/tweetlanes/android/core/ConsumerKeyConstants.java
@@ -0,0 +1,17 @@
+
+package com.tweetlanes.android.core;
+
+@SuppressWarnings("javadoc")
+public final class ConsumerKeyConstants {
+
+    /* This class is never initialized */
+    private ConsumerKeyConstants() {
+    }
+
+    public static final String TWITTER_CONSUMER_KEY = "ixaZxRLfpPW7rk1QZyym0Q";
+    public static final String TWITTER_CONSUMER_SECRET = "WGxzhWC8NwesWzQbvCdc9XBHjbaXBFOqYa09LS8xY";
+    public static final String APPDOTNET_CONSUMER_KEY = "Put your key here";
+    public static final String APPDOTNET_CONSUMER_SECRET = "Put your secret here";
+    public static final String CRITTERCISM_APP_ID = "4f6d53e0b093157b43000033";
+
+}
